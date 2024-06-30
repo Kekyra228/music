281e7a0b68629b2c09a235ef095a7c74bb31d1b1
Kekyra228 <katyiv02@gmail.com>
2024-06-30 17:35:01 +0400

    переустановила jest

--- src/app/components/navbar/logo.test.ts
+++ src/app/components/navbar/logo.test.ts
@@ -1,16 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Navbar from "./Navbar";
 
-// describe("Компонент Navbar", () => {
-//   it("лого должно корректно рендериться", () => {
-//     render(<Navbar />);
-//     const image = screen.getAllByAltText("navburger");
-//     expect(image).toBeInTheDocument();
-//   });
-// });
-
-test("navburger render ", () => {
-  render(<Navbar />);
-  const image = screen.getAllByAltText("navburger");
-  expect(image).toBeInTheDocument();
+describe("Компонент Navbar", () => {
+  it("лого должно корректно рендериться", () => {
+    render(<Navbar />);
+    const image = screen.getAllByAltText("navburger");
+    expect(image).toBeInTheDocument();
+  });
 });
+
+// test("navburger render ", () => {
+//   render(<Navbar />);
+//   const image = screen.getAllByAltText("navburger");
+//   expect(image).toBeInTheDocument();
+// });
